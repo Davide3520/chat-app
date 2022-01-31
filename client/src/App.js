9919586974bdc3018d91f8cd2b4d30af781a8001
Davide Lepri <davidelepri3@gmail.com>
2022-01-31 10:45:12 -0500

    fixed bugs

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
-import Join from './components/Join.js';
+import Join from './components/Join';
 import Chat from './components/Chat';
 
 const App = () => (
   <Router>
-    <Route path="/" exact component={Join}/>
-    <Route path="/char" component={Chat}/>
+    <Routes>
+    <Route path="/" exact element={<Join />}/>
+    <Route path="/chat" element={<Chat />}/>
+    </Routes>
   </Router>
 )
 
