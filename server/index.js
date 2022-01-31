a54b345f9d2bfe53f4c111ae6ae59871acb554a9
Davide Lepri <davidelepri3@gmail.com>
2022-01-31 11:31:17 -0500

    server route added

--- server/index.js
+++ server/index.js
@@ -3,10 +3,13 @@ const socketio = require('socket.io');
 const http = require('http');
 
 const PORT = process.env.PORT || 8080;
+const router = require('./router')
 const app = express();
 
 const server = http.createServer(app);
 const io = socketio(server);
+
+app.use(router)
 
 server.listen(PORT, () => {
   console.log(`Server is running in port ${PORT}`)
