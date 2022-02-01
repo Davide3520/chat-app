895a71e05bd020566d5af0da77e70e5eb5a0910a
Davide Lepri <davidelepri3@gmail.com>
2022-02-01 12:21:54 -0500

    socket added for client side

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,16 +2,22 @@ import React, {useState, useEffect} from "react";
 import queryString from 'query-string';
 import io from 'socket.io-client';
 
+let socket;
+
 const Chat = ({ location }) => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
-
+  const ENDPOINT = 'localhost:3000'
   useEffect(() => {
 
     const { name, room }=  queryString.parse(window.location.search)
+    socket = io(ENDPOINT);
     setName(name);
     setRoom(room)
-  })
+
+    socket.emit()
+
+  }, [ENDPOINT, window.location.search])
   return (
     <h1>CHATT</h1>
   )
