c11af71f4ebb3bd68e53612d9ac707495e3f32a3
Davide Lepri <davidelepri3@gmail.com>
2022-02-01 12:13:56 -0500

    useState added

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import queryString from 'query-string';
+import io from 'socket.io-client';
 
-const Chat = () => {
+const Chat = ({ location }) => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
+  useEffect(() => {
+
+    const { name, room }=  queryString.parse(window.location.search)
+    setName(name);
+    setRoom(room)
+  })
   return (
     <h1>CHATT</h1>
   )
--- server/index.js
+++ server/index.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const socketio = require('socket.io');
-const http = require('http');
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
 
 const PORT = process.env.PORT || 8080;
-const router = require('./router')
+const router = require("./router");
 const app = express();
 
 const server = http.createServer(app);
 const io = socketio(server); // instance of socketio
 
-io.on('connection', (socket) => {
-  console.log('We have a new connection!!');
+io.on("connection", (socket) => {
+  console.log("We have a new connection!!");
 
-  socket.on('disconnect', () => {
-    console.log('User had left!!');
-  })
-})
+  socket.on("disconnect", () => {
+    console.log("User had left!!");
+  });
+});
 
-app.use(router)
+app.use(router);
 
 server.listen(PORT, () => {
-  console.log(`Server is running in port ${PORT}`)
-})
+  console.log(`Server is running in port ${PORT}`);
+});
