74a1fa0e0c21987c339d87834619f9b2d14a888e
Davide Lepri <davidelepri3@gmail.com>
2022-02-01 13:26:09 -0500

    helper function done

--- server/users.js
+++ server/users.js
@@ -30,3 +30,10 @@ const removeUser = (id) => {
 const getUser = (id) => users.find((user) => user.id === id);
 
 const getUserInRoom = (room) => users.filter((user) => user.room === room);
+
+module.exports = {
+  addUser,
+  removeUser,
+  getUser,
+  getUserInRoom,
+};
