cbd5f080e9cde69d170ffcdd2f6367f034a0c929
Davide Lepri <davidelepri3@gmail.com>
2022-02-03 12:43:11 -0500

    message generator back end

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const { addUser, removeUser, getUser, getUserInRoom } = require('./users')
 
 const PORT = process.env.PORT || 8080;
 const router = require("./router");
+const { userInfo } = require("os");
 const app = express();
 
 const server = http.createServer(app);
@@ -17,9 +18,23 @@ io.on("connection", (socket) => {
 
     if (error) return callback(error);
 
+    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}`});
+
+    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name}, has joined`});
+
     socket.join(user.room);
 
+    callback();
+
   });
+
+  socket.on('sendMessage', (message, callback) => {
+
+    const user = getUser(socket.id);
+    io.to(user.room).emit('message', { user: user.name, text: message});
+    
+    callback();
+  })
 
   socket.on("disconnect", () => {
     console.log("User had left!!");
