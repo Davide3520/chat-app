ccd976680a19f2f137c2840f78e22aaec51f1ef8
Davide Lepri <davidelepri3@gmail.com>
2022-02-03 12:46:01 -0500

    listening for messages with socket.on

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -7,7 +7,10 @@ let socket;
 const Chat = ({ location }) => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
+  const [message, setMessage] = useState([]);
+  const [messages, setMessages] = useState('');
   const ENDPOINT = "localhost:8080";
+
   useEffect(() => {
     const { name, room } = queryString.parse(window.location.search);
     socket = io(ENDPOINT, { transports: ["websocket"] });
@@ -21,6 +24,13 @@ const Chat = ({ location }) => {
       socket.off();
     };
   }, [ENDPOINT, window.location.search]);
+
+  useEffect(() => {
+    socket.on('message', (message) => { // message is from backend
+
+    })
+  })
+
   return <h1>CHATT</h1>;
 };
 
